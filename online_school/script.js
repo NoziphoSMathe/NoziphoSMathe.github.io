64b9039e0867cfa6ef9c43b1f3b9526410e4c4d6
Nozipho Mathe <76788432+NoziphoSMathe@users.noreply.github.com>
2025-08-15 01:48:54 +0200

    Delete online_school directory

--- online_school/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const slides = document.querySelectorAll('.slide');
-    slides.forEach((slide, index) => {
-        slide.style.animationDelay = `${index * 0.2}s`;
-    });
-});
